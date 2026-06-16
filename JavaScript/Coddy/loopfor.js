476053a69a9b8d43fe89b29de4856b42513d75e1
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-06-16 19:23:52 -0300

    feat: Exercício Coddy (Desafio Loop For)

---
+++ JavaScript/Coddy/loopfor.js
@@ -0,0 +1,4 @@
+// Escreva um programa que imprima "Hello Coddy: " e o valor i de 3 a 27 (inclusive, 25 vezes no total), faça isso usando um loop for.
+for (let i = 3; i < 28; i++) {
+    console.log (`Hello Coddy: ${i}`)
+}
