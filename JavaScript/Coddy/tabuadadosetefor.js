b9d294ed03eb3f6fb60fc1ce1ea5bb0edcbba394
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-06-18 19:22:54 -0300

    feat: Exercícios Coddy tabuadadosete for e while

---
+++ JavaScript/Coddy/tabuadadosetefor.js
@@ -0,0 +1,5 @@
+const tabuada_Sete = 7
+for (i = 1; i < 11; i++) {
+    resultado = tabuada_Sete*i
+    console.log (`${tabuada_Sete} x ${i} = ${resultado}`)
+}
---
+++ JavaScript/Coddy/tabuadadosetewhile.js
@@ -0,0 +1,7 @@
+const tabuada_Sete = 7
+let contador_loop = 0
+while (contador_loop <= 9) {
+    contador_loop += 1
+    resultado = tabuada_Sete*contador_loop
+    console.log (`${tabuada_Sete} x ${contador_loop} = ${resultado}`)
+}
