3da8e70db131c70b80432f127e7ec84b5ac55396
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-06-19 18:42:52 -0300

    feat: Adicionei um desafio tabuadadousuario e umas explicações no arquivo de Obs e explicações

---
+++ Python/Beecrownd/Iniciantes/Tabuadausuario.py
@@ -0,0 +1,2 @@
+# Pensei num exercício aqui vou fazer é isso 
+# Basicamente o exercício consiste em pedir um número ao usuário e fazer a tabuada de qualquer número que ele digitar
---
+++ Python/Beecrownd/Obs ou explicações.py
@@ -0,0 +1,25 @@
+# myVariable = 2
+# Case sensitive => sensiveis a letras Maiusculas e Minusuculas
+# Snake_Case padrão
+# Escreva tudo em minusculas
+# myvariable = 2
+
+# x = 5
+# x = "10"
+# print (x)
+
+# if variavel == 3: (quando chegar no 3 sem incluir ele vai parar o programa vai finalizar)
+    # break
+
+# if variavel == 3: (quando chegar no 3 o programa vai continuar sem incluir o 3) 
+    # continue
+
+for i in range(1, 11 +1, +1): # posso pegar o 11 e somar e no final ele soma o valor ou pode ser ate uma variavel
+    if i == 3:
+        continue
+    print (i)
+
+for i in range (1, 11, +1):
+    if i == 3:
+        break
+    print (i)
