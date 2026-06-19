e59de101ad2e1fe3a5300e35dd49f3e15a62c951
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-06-19 19:29:04 -0300

    feat: Desafio Tabuada do Usuário

--- Python/Beecrownd/Iniciantes/Tabuadausuario.py
+++ Python/Beecrownd/Iniciantes/Tabuadausuario.py
@@ -1,2 +1,25 @@
 # Pensei num exercício aqui vou fazer é isso 
-# Basicamente o exercício consiste em pedir um número ao usuário e fazer a tabuada de qualquer número que ele digitar
+# Basicamente o exercício consiste em pedir um número ao usuário e fazer a tabuada de qualquer número que ele digitar tabuada de um a 10 é claro
+
+from time import sleep as baiano
+numero_usuario = int(input())
+
+# Usando for:
+for tabuada in range (1, 11, +1):
+    resultado = tabuada*numero_usuario
+    baiano (1)
+    print (f"{numero_usuario} x {tabuada} = {resultado}")
+
+# Usando while:
+
+contador = 0
+while (contador <= 9):
+    contador += 1
+    baiano (1)
+    resultado = contador*numero_usuario
+    print (f"{numero_usuario} x {contador} = {resultado}")
+    
+    
+
+
+
--- Python/Beecrownd/Iniciantes/tempCodeRunnerFile.py
+++ Python/Beecrownd/Iniciantes/tempCodeRunnerFile.py
@@ -1,9 +1,8 @@
-# Usando for 
-numero_usuario1 = int(input())
-if numero_usuario1 % 2 == 1: # se ele for impar
-    for impar in range(1, numero_usuario1, +2):
-        print (impar + 2)
-# Podia fazer um else aqui mas decidir fazer um elif para ficar mais legível
-elif numero_usuario1 % 2 == 0: # se ele for par
-    for impar in range(1, numero_usuario1, +2):
-        print (impar)
+from time import sleep as baiano
+# numero_usuario = int(input())
+
+# # Usando for:
+# for tabuada in range (1, 11, +1):
+#     resultado = tabuada*numero_usuario
+#     baiano (1)
+#     print (f"{numero_usuario} x {tabuada} = {resultado}")
