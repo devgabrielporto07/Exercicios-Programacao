11e6f13546f131b83c291a528cc69659058121e9
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-06-22 08:57:21 -0300

    Exercício Beecrownd 1000 (Hello World!.java)

---
+++ Java/Beecrownd/Iniciantes/HelloWorld!.java
@@ -0,0 +1,6 @@
+// Exercícios trazdios do IntelliJ uso essa IDE especificamente para Java em geral uso o VSCODE para outras linguagens
+public class HelloWorld! {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
