5fd8f4bd45198b76db70cc8274fc63b142772284
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-06-23 10:25:53 -0300

    fix: Erro na função consertado agora

--- JavaScript/Coddy/funçõesdesafio.js
+++ JavaScript/Coddy/funçõesdesafio.js
@@ -1,17 +1,13 @@
-// Write the function below
-const inp = 5
-function soma_Numbers () {
-    let valor = 0;
-    for (let valor = 1; valor <= 10000; valor++) {
-        valor += 1;
+function sumNumbers() {
+    let res = 0;
+    for (let i = 1; i <= 10000; i++) {
+        res += i;
     }
-    console.log (valor);
+    console.log(res);
 }
 
 let n = parseInt(inp); // Don't change this line
 
-// Execute your function n times with a loop
-
 for (let i = 0; i < n; i++) {
-    soma_Numbers ();
+    sumNumbers();
 }
