6a638e925e9dfc0f69d87e457c218369ae8f5ed7
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-07-01 19:35:39 -0300

    fix: Exercício Beecrowd 1015 Runtime Error (Distância Entre Dois Pontos.js)

--- JavaScript/Beecrownd/Iniciantes/Distância Entre Dois Pontos.js
+++ JavaScript/Beecrownd/Iniciantes/Distância Entre Dois Pontos.js
@@ -9,12 +9,12 @@ if (!linha2) return;
 const [valor1, valor2] = linha1.split(/\s+/);
 const [valor3, valor4] = linha2.split(/\s+/);
 
-let x1 = parseFloat(valor1);
-let x2 = parseFloat(valor3);
-let y1 = parseFloat(valor2);
-let y1 = parseFloat(valor4);
+let x_Um = parseFloat(valor1);
+let x_Dois = parseFloat(valor3);
+let y_Um = parseFloat(valor2);
+let y_Dois = parseFloat(valor4);
 
 // const distancia_Doispontos = ((x2 - x1)**2 + (y2 - y1))**(1/2); 
-const dist_Doispontos = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+const dist_Doispontos = Math.sqrt(Math.pow(x_Dois - x_Um, 2) + Math.pow(y_Dois - y_Um, 2));
 
 console.log(`${dist_Doispontos.toFixed(4)}`);
