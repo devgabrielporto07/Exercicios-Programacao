d445e1438f5ebefaa24f9de4976d3ef9d976572d
devgabrielporto07 <estudosgabriel07@gmail.com>
2026-07-27 22:19:06 -0300

    feat: Exercício 1035 (TestedeSelecao.java)

---
+++ Java/Beecrowd/Iniciantes/TestedeSelecao.java
@@ -0,0 +1,43 @@
+// IntelliJ:
+// Sintaxe das condicionais semelhante ao JavaScript
+package desafiosbeecrowd;
+import java.util.Locale;
+import java.util.Scanner;
+public class TestedeSelecao {
+    public static void main (String [] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner (System.in);
+        int valor_A = scanner.nextInt();
+        int valor_B = scanner.nextInt();
+        int valor_C = scanner.nextInt();
+        int valor_D = scanner.nextInt();
+        if (valor_B > valor_C && valor_D > valor_A && valor_C + valor_D > valor_A + valor_B && valor_C > 0 && valor_D > 0 && valor_A % 2 == 0) {
+            System.out.println ("Valores aceitos");
+        }
+        else {
+            System.out.println ("Valores nao aceitos");
+        }
+    }
+}
+
+// Terminal Beecrowd:
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner (System.in);
+        int valor_A = scanner.nextInt();
+        int valor_B = scanner.nextInt();
+        int valor_C = scanner.nextInt();
+        int valor_D = scanner.nextInt();
+        if (valor_B > valor_C && valor_D > valor_A && valor_C + valor_D > valor_A + valor_B && valor_C > 0 && valor_D > 0 && valor_A % 2 == 0) {
+            System.out.println ("Valores aceitos");
+        }
+        else {
+            System.out.println ("Valores nao aceitos");
+        }
+    }
+}
